cf9483d5d1ae6433ef37f6382fef825a104d9a5c
sixtycode <sagaraperkasa.work@gmail.com>
2024-09-26 21:05:05 +0700

    refactor: remove label every input

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,37 @@ function App() {
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
-        <label for="default-checkbox" className="ms-2 text-sm font-medium dark:text-gray-300">Create Guest Experience mobile check-in</label>
+        Create Guest Experience mobile check-in
         <button><FaTrashAlt /></button>
       </div>
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
-        <label for="default-checkbox" className="ms-2 text-sm font-medium dark:text-gray-300">Document current CI/CD process</label>
+        Document current CI/CD process
         <button><FaTrashAlt /></button>
       </div>
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
-        <label for="default-checkbox" className="ms-2 text-sm font-medium dark:text-gray-300">Perform Code Review for final Pillow-Talk release</label>
+        Perform Code Review for final Pillow-Talk release
         <button><FaTrashAlt /></button>
       </div>
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
-        <label for="default-checkbox" className="ms-2 text-sm font-medium dark:text-gray-300">Implement new Color Palette from Design Team</label>
+        Implement new Color Palette from Design Team
         <button><FaTrashAlt /></button>
       </div>
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
-        <label for="default-checkbox" className="ms-2 text-sm font-medium dark:text-gray-300">Fix image uploading process for guest check-in</label>
+        Fix image uploading process for guest check-in
         <button><FaTrashAlt /></button>
       </div>
       <div className="flex items-center mb-4">
         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
         </input>
-        <label for="default-checkbox" className="ms-2 text-sm font-medium dark:text-gray-300">Provide on-boarding documentation</label>
+        Provide on-boarding documentation
         <button><FaTrashAlt /></button>
       </div>
       <p>-----------------------------------------------------------------------</p>
